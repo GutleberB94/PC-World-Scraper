f4befe6d93e7d9b8af651f77f4fd19f6deda021a
GutleberB94 <gutleberb@gmail.com>
2019-05-22 14:12:10 -0500

    setup contoller to load landing page

---
+++ controllers/scraperController.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Require all models
+//var db = require("./models");
+
+router.get("/", (req, res) =>{
+
+    res.render("index")
+})
+
+
+// Export routes for server.js to use
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
-
-// Require all models
-var db = require("./models");
 
 var PORT = 3000;
 
@@ -25,6 +23,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
+// Set Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/scraperController.js");
+
+app.use(routes);
 
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
