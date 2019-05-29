2a8448c3b65e9fc558d1b0171c77ab15ce2206f7
Brendan G <gutleberb@outlook.com>
2019-05-29 14:15:12 -0500

    setup loop to scrape website

--- controllers/scraperController.js
+++ controllers/scraperController.js
@@ -1,14 +1,38 @@
 var express = require("express");
 
+var axios = require("axios");
+var cheerio = require("cheerio");
+
 var router = express.Router();
 
 // Require all models
 //var db = require("./models");
 
-router.get("/", (req, res) =>{
+router.get("/", (req, res) => {
 
     res.render("index")
 })
+
+// A GET route for scraping the echoJS website
+router.get("/scrape", function (req, res) {
+
+    axios.get("https://www.pcworld.com/news/").then(response => {
+
+        var $ = cheerio.load(response.data);
+
+        let articles = $(".landing-listing")
+
+        console.log(articles.children.html())
+
+        $("excerpts p").each(function(i, element) {
+            // Save an empty result object
+            var result = {};
+
+        });
+        // Send a message to the client
+        res.send("Scrape Complete");
+    });
+});
 
 
 // Export routes for server.js to use
