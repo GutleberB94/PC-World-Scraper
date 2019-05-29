2f518f1eaa283b92e997876fda8215edb4ab0916
GutleberB94 <gutleberb@gmail.com>
2019-05-29 16:15:32 -0500

    linked js and css files to handlebars

---
+++ public/js/scraper.js
@@ -0,0 +1,7 @@
+$.getJSON("/api/articles", (data) => {
+
+    for (var i = 0; i < data.length; i++) {
+        // Display the apropos information on the page
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].headline + "<br />" + data[i].summary + "<br />"+ data[i].link + "</p>");
+      }
+})
